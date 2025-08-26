de131da33a607df0b4efec6840f78f47eec55597
ju4ncb <ju4ncb@gmail.com>
2025-08-26 02:02:53 -0500

    updated logo

--- renderer/+onRenderHtml.tsx
+++ renderer/+onRenderHtml.tsx
@@ -4,7 +4,7 @@ export { onRenderHtml };
 import ReactDOMServer from "react-dom/server";
 import { PageShell } from "./PageShell";
 import { escapeInject, dangerouslySkipEscape } from "vike/server";
-import logoUrl from "../public/logo.png";
+import logoUrl from "../public/logo.ico";
 import type { OnRenderHtmlAsync } from "vike/types";
 import { getPageTitle } from "./getPageTitle";
 
@@ -38,7 +38,7 @@ const onRenderHtml: OnRenderHtmlAsync = async (
     <html lang="en">
       <head>
         <meta charset="UTF-8" />
-        <link rel="icon" href="${logoUrl}" />
+        <link rel="icon" href="${logoUrl}" type="image/x-icon" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="${desc}" />
