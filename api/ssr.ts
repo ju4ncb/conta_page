ed494ea2d1395a5bb8e39e7899a0d0b7e230ddb7
ju4ncb <ju4ncb@gmail.com>
2025-08-25 23:15:56 -0500

    changed structure

--- api/ssr.ts
+++ api/ssr.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { handleRequest } from "./api/render";
+import { handleRequest } from "../server/render";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   await handleRequest(req, res);
